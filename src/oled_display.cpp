d4d9cbf3294f5fb4f4097be735f98a6fd535191a
Arie Lev <levinsonarie@gmail.com>
2020-04-25 15:39:54 +0300

    update documentation

--- src/oled_display.cpp
+++ src/oled_display.cpp
@@ -9,6 +9,10 @@
 
 #include "oled_display.h"
 
+
+/**
+ * print 'nalkinsBitmap' logo to display
+ */
 void Oled64x48Display::displayLogo() {
 	display.clearDisplay();
 	// Display nalkisncloud on start, aligned with 64x48 display
@@ -25,6 +29,11 @@ void Oled64x48Display::displayLogo() {
 	display.display();
 }
 
+/**
+ * print temperature and humidity to display in 4 separate lines
+ * @param temp temperature to print
+ * @param humidity humidity to print
+ */
 void Oled64x48Display::displayTemp(float temp, float humidity) {
 	display.clearDisplay();
 	display.setCursor(0,0);
@@ -36,15 +45,24 @@ void Oled64x48Display::displayTemp(float temp, float humidity) {
 	display.display();
 }
 
+/**
+ * init ith the I2C addr 0x3C (for 64x48),
+ * set relevant text size and color
+ */
 void Oled64x48Display::initDisplay() {
-	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for 64x48)
+	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize w
 	display.display();
 	delay(1000);
 	display.setTextSize(textSize);
 	display.setTextColor(textColor);
 }
 
-
+/**
+ * init new Oled64x48Display object, full example can be found here https://github.com/adafruit/Adafruit_SSD1306
+ *
+ * @param _textSize text side to be used for current object
+ * @param _textColor text color to be used for current object
+ */
 Oled64x48Display::Oled64x48Display(uint8_t _textSize, uint8_t _textColor) {
 	textSize = _textSize;
 	textColor = _textColor;
